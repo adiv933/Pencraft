082823e14fbe9400d1ab42528b5b0a0bf23ccbe8
Aditya Verma <adityav090304@gmail.com>
2025-06-02 03:11:00 +0530

    Change fe env vars for prod

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-const NODE_ENV: string = 'dev';
+const NODE_ENV: string = 'prod';
 export const BACKEND_URL = NODE_ENV == 'prod' ? "https://server.adityav090304.workers.dev/api/v1" : "http://localhost:8787/api/v1"

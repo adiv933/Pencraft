078107b8f062d6f65cd51998f1c098de0be031a8
Aditya Verma <adityav090304@gmail.com>
2025-06-02 03:24:06 +0530

    Replace default route on fe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
-import { Publish } from './pages/Publish';
-import { Post } from './pages/Post';
-import { Posts } from './pages/Posts';
-import { UpdatePost } from './pages/UpdatePost';
+import { Publish } from './pages/Publish'
+import { Post } from './pages/Post'
+import { Posts } from './pages/Posts'
+import { UpdatePost } from './pages/UpdatePost'
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="/post/edit/:id" element={<UpdatePost />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/edit/:id" element={<UpdatePost />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/publish" element={<Publish />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
